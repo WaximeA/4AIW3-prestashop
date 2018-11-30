e9f9b2cd9607ec9d485e9c749e19768479ea2a4c
waxime <aveline.maxime@gmail.com>
2018-11-30 11:48:57 +0100

    Init Prestashop

---
+++ src/app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'mariadb',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => 'root',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'OfIGY8R2FneL8pXXPRWSQ3D3NH35DK43OcTMMWAD2fzyavwa4UR6CYM9',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-11-30',
+    'locale' => 'fr-FR',
+    'cookie_key' => 'I5ZbQEQRwsxREd1swiLt9tBHFFzdLziU2xBXsrzwea8EkzUEOcvzxjno',
+    'cookie_iv' => 'naIO41in',
+    'new_cookie_key' => 'def0000061ba33db78bbfb8038adfd4775cff76a4c1b468ed4c763918824e05e1bd9e0cf1179857e1fc9f5016672b21041c979584eb40d0e0adcdb73067cd8b3abe021fc',
+  ),
+);
